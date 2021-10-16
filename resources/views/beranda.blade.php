7b2e486e1810a531f109babbdd9caf3354514002
Eky Galih <ekkygalih8@gmail.com>
2021-10-16 10:51:57 +0800

    buat readme.md

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -5,7 +5,7 @@ Dompet Eky Galih Gunanda
 @section('content')
 <div class="jumbotron">
     <h1 class="display-4">Hallo, Atomic!</h1>
-    <p class="lead">Aplikasi yang dinamakan "dompet" untuk management keuangan kamu.</p>
+    <p class="lead">Ini adalah Aplikasi yang dinamakan "dompet" untuk management keuangan kamu.</p>
     <hr class="my-4">
     <p>Terima kasih atas kesempatan dan waktu yang telah diberikan untuk mengikuti tes ini. <br/> Salam Hormat<br/>Eky Galih Gunanda</p>
     <a class="btn btn-primary btn-lg" href="{{ route('dompet') }}" role="button">Mulai</a>
